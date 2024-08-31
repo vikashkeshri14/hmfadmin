2da6625f3f88c1b823014bb45ae93a01139883d5
Vikash Keshri <vkeshri.14@gmail.com>
2024-08-31 04:54:09 +0300

    update

--- src/component/user/PaymentContent.js
+++ src/component/user/PaymentContent.js
@@ -6,6 +6,8 @@ import DatePicker from "react-datepicker";
 export default function PaymentContent() {
   const [userId, setUserId] = useState("");
   const [allpayments, setallpayments] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [storeDetail, setstoreDetail] = useState([]);
   useEffect(() => {
     const auth = JSON.parse(localStorage.getItem("loginUser"));
     setUserId(auth.id);
@@ -18,6 +20,18 @@ export default function PaymentContent() {
     console.log(response);
     setallpayments(response.result);
     //setinitialdata(response.result);
+  };
+
+  const paidFamily = async (args) => {};
+  const getOrderDetail = async (args) => {
+    const obj = {
+      order_id: args,
+    };
+    let params = { url: apiList.getStoreByOrderId, body: obj };
+    let response = await ApiService.postData(params);
+    setstoreDetail(response.result);
+    console.log(response);
+    setShowModal((showModal) => !showModal);
   };
   return (
     <div className="app-content  content">
@@ -34,16 +48,26 @@ export default function PaymentContent() {
               </div>
             </div>
             <div className="min-h-[450px]">
-              {allpayments.length > 0 &&
-                allpayments.map((data, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className="row p-[10px]  bg-white rounded-[6px] mr-[0px] mt-[10px]"
-                    >
-                      <table className="table mb-0">
-                        <tbody>
+              <div className="row p-[10px]  bg-white rounded-[6px] mr-[0px] mt-[10px]">
+                <table className="table mb-0">
+                  <thead>
+                    <tr>
+                      <th className="text-center ">Order Id</th>
+                      <th className="text-center ">User</th>
+                      <th className="text-center ">Store</th>
+                      <th className="text-center ">Payment Date</th>
+                      <th className="text-center ">Payment Id</th>
+                      <th className="text-center ">Payment Method</th>
+                      <th className="text-center ">Family Payment Status</th>
+                      <th className="text-center ">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {allpayments.length > 0 &&
+                      allpayments.map((data, i) => {
+                        return (
                           <tr
+                            key={i}
                             style={{
                               borderRightWidth: 0,
                             }}
@@ -51,7 +75,13 @@ export default function PaymentContent() {
                           >
                             <td
                               style={{ borderLeftWidth: 1 }}
-                              className=" w-[15%] text-center "
+                              className="text-center "
+                            >
+                              {data.id}
+                            </td>
+                            <td
+                              style={{ borderLeftWidth: 1 }}
+                              className="text-center "
                             >
                               <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
                                 {data.username}
@@ -59,7 +89,7 @@ export default function PaymentContent() {
                             </td>
                             <td
                               style={{ borderLeftWidth: 1 }}
-                              className=" w-[15%] text-center "
+                              className="text-center "
                             >
                               <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
                                 {data.storename}
@@ -67,54 +97,128 @@ export default function PaymentContent() {
                             </td>
                             <td
                               style={{ borderLeftWidth: 1 }}
-                              className=" w-[15%] text-center "
+                              className="text-center "
                             >
-                              <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
-                                {data.total}
-                              </div>
+                              {moment(data.payment_date).format(
+                                "DD/MM/YYYY HH:mm"
+                              )}
                             </td>
+
                             <td
                               style={{ borderLeftWidth: 1 }}
-                              className=" w-[15%] text-center "
-                            >
-                              <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
-                                Order ID:: {data.id}
-                              </div>
-                            </td>
-                            <td
-                              style={{ borderLeftWidth: 1 }}
-                              className="w-[15%]  text-center "
-                            >
-                              <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
-                                {moment(data.created_at).format("DD/MM/YYYY")}
-                              </div>
-                            </td>
-                            <td
-                              style={{ borderLeftWidth: 0 }}
-                              className="w-[15%]  text-center "
+                              className=" text-center "
                             >
                               <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
                                 {data.payment_id}
                               </div>
                             </td>
                             <td
-                              style={{ borderLeftWidth: 0 }}
-                              className="w-[15%]  text-center "
+                              style={{ borderLeftWidth: 1 }}
+                              className="text-center "
                             >
-                              <div className=" flex justify-center text-[#484848] text-[16px] font-sstbold ">
-                                {moment(data.payment_date).format("DD/MM/YYYY")}
-                              </div>
+                              {data.provider_method}
+                            </td>
+                            <td
+                              style={{ borderLeftWidth: 1 }}
+                              className="text-center "
+                            >
+                              {data.family_paid_status == 0 ? (
+                                <span className="text-warning">Pending</span>
+                              ) : (
+                                <span className="text-success">Pending</span>
+                              )}
+                            </td>
+                            <td
+                              style={{ borderLeftWidth: 1 }}
+                              className="text-center "
+                            >
+                              {data.family_paid_status == 0 && (
+                                <div
+                                  className="text-primary cursor-pointer"
+                                  onClick={() => getOrderDetail(data.id)}
+                                >
+                                  Pay
+                                </div>
+                              )}
                             </td>
                           </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  );
-                })}
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </section>
         </div>
       </div>
+      {showModal && (
+        <>
+          <div className="justify-center items-center flex   fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative  max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-[900px] bg-[#FAFAFA] outline-none focus:outline-none">
+                <div className="card h-[300px] bg-[#FAFAFA] mt-[10px]">
+                  <div className="card-body">
+                    <form className="form form-horizontal">
+                      <div className="form-body">
+                        <div className="row">
+                          <div className="col-md-12 text-[#484848] text-center text-[16px] font-sstbold pl-[0px] pr-[0px]">
+                            تفاصيل المتجر
+                          </div>
+                          <div className="col-md-12">
+                            <table className="table mb-0">
+                              <thead>
+                                <tr>
+                                  <th className="text-center ">Order Id</th>
+                                  <th className="text-center ">Store User</th>
+                                  <th className="text-center ">Stc Number</th>
+                                  <th className="text-center ">
+                                    Bank A/c Name
+                                  </th>
+                                  <th className="text-center ">
+                                    Account Number
+                                  </th>
+                                  <th className="text-center ">Iban</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {storeDetail.length > 0 && (
+                                  <tr>
+                                    <td>{storeDetail[0].order_id}</td>
+                                    <td>{storeDetail[0].username}</td>
+                                    <td>{storeDetail[0].stc_number}</td>
+                                    <td>
+                                      {storeDetail[0].bank_account_fullname}
+                                    </td>
+                                    <td>{storeDetail[0].account_number}</td>
+                                    <td>{storeDetail[0].iban}</td>
+                                  </tr>
+                                )}
+                              </tbody>
+                            </table>
+                          </div>
+                          <div className="col-md-12">
+                            <button
+                              onClick={() => {
+                                paidFamily();
+                              }}
+                              type="button"
+                              class="btn hover:text-[#707070] send bg-[#959494] text-[24px] w-[120px] h-[48px] rounded-[6px] font-sstbold text-[#ffffff] mr-1"
+                            >
+                              Paid
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      )}
     </div>
   );
 }
